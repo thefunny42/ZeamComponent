9f85a54414153b10e7c467c82b8528fdc2b0eef2
Sylvain Viollon <thefunny@gmail.com>
2012-03-16 11:58:53 +0100

    Skeleton.

---
+++ README.txt
@@ -0,0 +1,7 @@
+==============
+zeam.component
+==============
+
+``zeam.component`` is inspired by ``zope.component`` and
+``grokcore.component``, but stays much simpler, and have less
+concept. Hopefully this will improve flexibility.
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+tests_require = [
+    ]
+
+setup(name='zeam.component',
+      version=version,
+      description="Component infrastructure, based on zope.interface",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='component interface',
+      author='Sylvain Viollon',
+      author_email='thefunny@gmail.com',
+      url='http://pypi.python.org/pypi/zeam.component',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['zeam'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'martian',
+          ],
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
